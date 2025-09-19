0afa6b4c30f7caba4eb188c770c48b8834b4ae59
Emmanuel Yirenkyi - Danso <emmanuelyirenkyidanso1991@gmail.com>
2025-09-19 03:34:08 +0000

    use only image for logo and make it bigger and clearer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link';
 import { GhanaMustGoIcon } from '@/components/icons';
 
@@ -7,8 +8,7 @@ export default function Footer() {
       <div className="container flex flex-col items-center justify-between gap-4 py-6 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Link href="/" className="flex items-center space-x-2">
-            <GhanaMustGoIcon className="h-8 w-auto text-primary" />
-            <span className="font-bold sm:inline-block font-headline text-2xl">Ghana Must Go</span>
+            <GhanaMustGoIcon className="h-10 w-auto text-primary" />
           </Link>
         </div>
         <p className="text-center text-sm text-muted-foreground md:text-left">
