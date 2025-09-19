be26ada91d61dd2d5ad3955345806596083a28ee
Emmanuel Yirenkyi - Danso <emmanuelyirenkyidanso1991@gmail.com>
2025-09-19 03:42:51 +0000

    And adjust to look professional

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -52,11 +52,11 @@ export const MopedIcon = (props: React.SVGProps<SVGSVGElement>) => (
       strokeLinejoin="round"
       {...props}
     >
-        <path d="M5 16.5c-1.5 0-3 1.5-3 3s1.5 3 3 3 3-1.5 3-3-1.5-3-3-3zM19 16.5c-1.5 0-3 1.5-3 3s1.5 3 3 3 3-1.5 3-3-1.5-3-3-3z"/>
-        <path d="M8 19h8"/>
-        <path d="M19 14a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2z"/>
-        <path d="M5 11v-5h8"/>
-        <path d="M11 6L7 4"/>
-        <path d="M13 11V4h-2"/>
+      <path d="M5 16.5c-1.5 0-3 1.5-3 3s1.5 3 3 3 3-1.5 3-3-1.5-3-3-3zM19 16.5c-1.5 0-3 1.5-3 3s1.5 3 3 3 3-1.5 3-3-1.5-3-3-3z"/>
+      <path d="M8 19h8"/>
+      <path d="M19 14a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2z"/>
+      <path d="M5 11v-5h8"/>
+      <path d="M11 6L7 4"/>
+      <path d="M13 11V4h-2"/>
     </svg>
 );
