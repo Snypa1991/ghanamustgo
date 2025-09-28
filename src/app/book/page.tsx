7fd4f203f939170fca3d1e4098f3342d9d6b93fe
Emmanuel Yirenkyi - Danso <emmanuelyirenkyidanso1991@gmail.com>
2025-09-28 03:07:25 +0000

    And "Car" to "Taxi"

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -92,7 +92,7 @@ export default function BookPage() {
                                 className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
                             >
                                 <Car className="mb-3 h-6 w-6" />
-                                Car
+                                Taxi
                             </Label>
                         </div>
                     </RadioGroup>
