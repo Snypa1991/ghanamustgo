23ebfb6fdcd576f70ec41e272f1bb0fe61c62e3f
Emmanuel Yirenkyi - Danso <emmanuelyirenkyidanso1991@gmail.com>
2025-09-28 03:06:50 +0000

    Change "Motorcycle" to "Okada"

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -82,7 +82,7 @@ export default function BookPage() {
                                 className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
                             >
                                 <MopedIcon className="mb-3 h-6 w-6" />
-                                Motorcycle
+                                Okada
                             </Label>
                         </div>
                          <div>
